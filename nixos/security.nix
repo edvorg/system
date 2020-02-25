19c7a2801b4e313e4389c060f07351559fe39fd7
Eduard Knyshov <edvorg@gmail.com>
2020-02-25 15:34:52 +0800

    enable ssh over gnupg

--- nixos/security.nix
+++ nixos/security.nix
@@ -7,6 +7,12 @@
   programs.ssh.askPassword = "/run/current-system/sw/bin/ksshaskpass";
   programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
 
+  environment.shellInit = ''
+    export GPG_TTY="$(tty)"
+    gpg-connect-agent /bye
+    export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
+  '';
+
   environment.systemPackages = with pkgs; [
     gnupg
     ksshaskpass
