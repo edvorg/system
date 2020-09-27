dcfdeea496ab9e76c40d77d9dd9ad35626210f45
Eduard Knyshov <edvorg@gmail.com>
2020-09-27 18:23:07 +0800

    add xps config

---
+++ nixos/xps-specific.nix
@@ -0,0 +1,18 @@
+{ config, pkgs, ... }:
+
+{
+  boot.initrd.luks.devices = {
+    root = {
+      name = "root";
+      device = "/dev/disk/by-uuid/cb026bd3-c1cf-4f9a-9897-c894d69168a4";
+      preLVM = true;
+      allowDiscards = true;
+    };
+  };
+
+  networking.hostName = "edvorg-xps"; # Define your hostname.
+
+  services.xserver.videoDrivers = [ "intel" ];
+
+  hardware.opengl.driSupport32Bit = true;
+}
