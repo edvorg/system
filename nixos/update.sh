a2c32fb90279a56f414933b2a59539dc95a08dcd
edvorg <edvorg@gmail.com>
2024-01-21 11:16:37 +0700

    refactor

--- nixos/update.sh
+++ nixos/update.sh
@@ -5,7 +5,7 @@ set -e
 if [ -z "${1}" ] ; then echo target was not provided ; exit 1 ; fi
 
 echo updating
-sudo cp -fv ~/Projects/system/nixos/${1}-specific.nix /etc/nixos/hardware-specific.nix
+sudo cp -fv ~/Projects/system/nixos/hardware-specific/${1}.nix /etc/nixos/hardware-specific.nix
 sudo cp -fv ~/Projects/system/nixos/configuration.nix /etc/nixos/configuration.nix
 sudo cp -fv ~/Projects/system/nixos/security.nix /etc/nixos/security.nix
 sudo cp -fv ~/Projects/system/nixos/kde.nix /etc/nixos/kde.nix
