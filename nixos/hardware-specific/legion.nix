319e56a996bacbfaeb521600fa46e1ca4765224f
edvorg <edvorg@gmail.com>
2024-01-21 21:56:37 +0700

    move graphics setup to hardware specific

--- nixos/hardware-specific/legion.nix
+++ nixos/hardware-specific/legion.nix
@@ -1,18 +1,45 @@
 { config, pkgs, ... }:
 
 {
-  boot.initrd.luks.devices = {
-    root = {
-      name = "root";
-      device = "/dev/disk/by-uuid/cb026bd3-c1cf-4f9a-9897-c894d69168a4";
-      preLVM = true;
-      allowDiscards = true;
-    };
-  };
-
   networking.hostName = "edvorg-legion-linux"; # Define your hostname.
 
-  services.xserver.videoDrivers = [ "intel" ];
+  # graphics
 
-  hardware.opengl.driSupport32Bit = true;
+  # Enable OpenGL
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  };
+
+  # Load nvidia driver for Xorg and Wayland
+  services.xserver.videoDrivers = ["nvidia"];
+
+  hardware.nvidia = {
+
+    # Modesetting is required.
+    modesetting.enable = true;
+
+    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+    powerManagement.enable = false;
+    # Fine-grained power management. Turns off GPU when not in use.
+    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+    powerManagement.finegrained = false;
+
+    # Use the NVidia open source kernel module (not to be confused with the
+    # independent third-party "nouveau" open source driver).
+    # Support is limited to the Turing and later architectures. Full list of
+    # supported GPUs is at:
+    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
+    # Only available from driver 515.43.04+
+    # Currently alpha-quality/buggy, so false is currently the recommended setting.
+    open = false;
+
+    # Enable the Nvidia settings menu,
+	# accessible via `nvidia-settings`.
+    nvidiaSettings = true;
+
+    # Optionally, you may need to select the appropriate driver version for your specific GPU.
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
 }
